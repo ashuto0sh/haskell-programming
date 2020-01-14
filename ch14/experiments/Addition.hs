12c9fcce3da0b5f288051f8ddbbea7efa9939926
Ashutosh Tripathi <astripat@microsoft.com>
2020-01-14 19:40:17 +0530

    Mnor changes

--- ch14/experiments/Addition.hs
+++ ch14/experiments/Addition.hs
@@ -19,6 +19,37 @@ multiplyBy a b =
             | n == 0    = r
             | otherwise = helper m (n-1) (r+a)
 
+trivialInt :: Gen Int
+trivialInt = return 1
+
+oneThroughThree :: Gen Int
+oneThroughThree = elements [1, 2, 3]
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = do
+    a <- arbitrary
+    b <- arbitrary
+    return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (a, b, c)
+genThreeple = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (a, b, c)
+
 main :: IO ()
 main = hspec $ do
     describe "Addition" $ do 
